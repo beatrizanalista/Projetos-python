6850a6b3c27b7205aa2233600dd974073975d452
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-04-12 21:23:20 -0300

    hello world

---
+++ aula001_primeiro_programa/aula1.py
@@ -0,0 +1,19 @@
+#curso de desenvolvimento de sistemas
+#turma 0152 (braba)
+#autor: beatriz victoria
+#data:12/04/2024
+#primeiro programa python
+
+#importanto as bibliotecas
+import os
+
+
+#limpando o terminal
+os.system('cls')
+
+#o comando print() executa uma saida via terminal
+
+print('-' * 70)
+print('hello world!!!')
+print('-' * 70)
+print()
