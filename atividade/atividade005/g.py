ec2f75715ce477fe5e7e00193c247c86a31c2e79
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-06-03 17:25:38 -0300

    atividade005

---
+++ atividade/atividade005/g.py
@@ -0,0 +1,6 @@
+# curso de desenvolvimento de sistemas
+# turma 0152 
+# autor:Beatriz Victoria
+# data: 31/06/2024
+# atividade 004
+#Faça um programa que calcule os números primos em um intervalo pré-determinado pelo usuário.
--- atividade004_strings/h.py
+++ atividade004_strings/h.py
@@ -1,9 +1,26 @@
+# curso de desenvolvimento de sistemas
+# turma 0152 
+# autor:Beatriz Victoria
+# data: 21/05/2024
+# atividade 004
+# Faça um programa que leia o nome de um aluno e mostre quantas vezes a letra 'o' aparece e em que posição ela aparece pela primeira e última vez
+
 import os
 
 os.system('cls')
 
+print('-'*70)
+print('digite o nome completo')
+print('.'*70)
 
-frase = str(input('digite uma frase: '))
-print('A letra A aparece {} vezes na frase.'.format(frase.count('A')))
-print('A primeira letra aparece na pocicao {}'.informat(frase.find('A')+1))
-print('A ultima palavra letra A aparece opocicao {}'.format(frase.rfind('A')+1))
+print('-'*70)
+nome_completo = str(input('entre com o nome: ' )).strip()
+print('.'*70)
+
+lista_quebrado = nome_completo.split(' ')
+primeiro_nome = lista_quebrado[0]
+ultimo_nome = lista_quebrado[-1]
+
+print(lista_quebrado)
+print(f'seu primeiro nome é {primeiro_nome} ')
+print(f'seu ultimo nome é {ultimo_nome} ') 
