d1ac8707bd4b43c8226bf149a1a3ace0fff005f1
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-06-24 21:39:02 -0300

    issubset014

---
+++ atividade/atividade008_set/issubset.py
@@ -0,0 +1,29 @@
+# Trabalho sobre a estrutura de dados SET
+# Senac Minas Gerais/Juiz de Fora
+# Aluno:Beatriz Victoria
+# Turma: 0152
+# Ano: 2024
+
+# (Issubset)-Verifica se o set é um subconjunto de outro set.
+
+# iniciando 
+import os
+
+
+os.system('cls')
+
+print('-'*70)
+print('utilizando o issubset ')
+print('='*70)
+
+# iniciando um set com elementos  # fruts
+
+set1 = [banana, mango, apple, orange, stanberry, bluberry,]
+set2 = [mango, apple stranberry, ] # saida false
+set3 = [a, h, e] # saida false
+
+# pedindo para o usuario entrar com as informações
+ 
+
+
+
