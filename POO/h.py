ffe79cdd2583b20c2d0fa22ae90410de7cd99ebb
beatrizanalista <beatriz.vivi0310@gmail.com>
2024-09-11 21:30:29 -0300

    continuando-atividade 02

--- POO/h.py
+++ POO/h.py
@@ -9,18 +9,44 @@ import os
 
 os.system('cls')
 
-comeco = int(input('digite o valor do primeiro intervalo:'))
-fim = int(input('digite o valor do segundo intervalo:'))
-print('3 valores irão ser ignorados')
+
+class intervalo:
+    def __init__(self, ignorado_1, ignorado_2, ignorado_3, inicio, fim):
+        self.ignorado_1 = ignorado_1
+        self.ignorado_2 = ignorado_2
+        self.ignorado_3 = ignorado_3
+        self.inicio = inicio
+        self.fim = fim
+        
+    def impressos_ignorado(self):
+        pass
+    
+    
+class Ignorado(intervalo):
+    def __init__(self, inicio, fim, ignorado_1, ignorado_2, ignorado_3):
+        self.ignorado_1 = ignorado_1
+        self.ignorado_2 = ignorado_2
+        self.ignorado_3 = ignorado_3
+        self.inicio = inicio
+        self.fim = fim
+        
+    def impressos_ignorado(self):
+        for i in range(self.inicio, self.fim):
+            if i == ignorado_1 or i == ignorado_2 or i == ignorado_3:
+                continue
+            else:
+                print(i, end=', ')
+        
 ignorado_1= int(input('entre com o 1 valor: '))
 ignorado_2 = int(input('entre com o 2 valor: '))
 ignorado_3 = int(input('entre com o 3 valor: '))
+inicio = int(input('o primeiro número para iniciar,onde será ignorado: '))
+fim = int(input('o ultimo (fim) número para finalizar,onde será ignorado: '))
+print('3 valores irão ser ignorados')
 
-for i in range(comeco, fim):
-    if i ==  ignorado_1 or i == ignorado_2 or i == ignorado_3:
-        continue
-print('')
-print({i})
+imprimir = Ignorado(inicio, fim, ignorado_1, ignorado_2, ignorado_3,)
+imprimir.impressos_ignorado()
+print()
 
 print('-'*70)
 print('fim do programa!')
